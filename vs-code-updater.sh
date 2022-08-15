c56e41344bbb9b07053249e713218f2da11e5f88
Charbel <99130418+CharbelElBateh@users.noreply.github.com>
2022-08-15 11:04:43 +0300

    Update vs-code-updater.sh

--- vs-code-updater.sh
+++ vs-code-updater.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Code by CharbelElBateh
+#Github: www.github.com/CharbelElBateh
+#Email: charbelelbateh@gmail.com
+
+#Downloading file
 wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O /tmp/code_latest_amd64.deb
 
+#Installing file
 sudo dpkg -i /tmp/code_latest_amd64.deb
